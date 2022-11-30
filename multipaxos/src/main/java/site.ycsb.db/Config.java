1dfe83552840ef51164183236ed6ea964e3bac8e
Zhiying Liang <zvl5490@psu.edu>
2022-11-29 22:30:43 -0500

    add multipaxos client

---
+++ multipaxos/src/main/java/site.ycsb.db/Config.java
@@ -0,0 +1,31 @@
+package site.ycsb.db;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class Config {
+  @JsonProperty("LeaderID")
+  private int leaderId;
+  @JsonProperty("Address")
+  private List<String> serverAddresses;
+
+  public int getLeaderId() {
+    return leaderId;
+  }
+
+  public String getServerAddress(int id) {
+    return serverAddresses.get(id);
+  }
+
+  public String getLeaderAddress() {
+    return serverAddresses.get(leaderId);
+  }
+
+  public List<String> getAllServerAddresses() {
+    return serverAddresses;
+  }
+}
---
+++ multipaxos/src/main/java/site.ycsb.db/MultipaxosClient.java
@@ -0,0 +1,108 @@
+package site.ycsb.db;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import site.ycsb.*;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+
+/**
+ *
+ */
+public class MultipaxosClient extends DB {
+  private Socket socket;
+  private Config config;
+  private PrintWriter writer;
+  private BufferedReader reader;
+
+  @Override
+  public void init() throws DBException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      config = objectMapper.readValue(
+          new File("config.json"),
+          Config.class);
+    } catch (IOException e) {
+      System.err.println("Couldn't load config.json");
+      System.exit(1);
+    }
+    String serverAddress = config.getLeaderAddress();
+    String[] tokens = serverAddress.split(":");
+    String ip = tokens[0];
+    int port = Integer.parseInt(tokens[1]);
+
+    while (true) {
+      try {
+        socket = new Socket(ip, port);
+        writer = new PrintWriter(socket.getOutputStream(), true);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        break;
+      } catch(Exception e){
+        System.out.println(e.getMessage());
+      }
+    }
+  }
+
+  //Read a single record
+  @Override
+  public Status read(final String table, final String key, final Set<String> fields,
+                     final Map<String, ByteIterator> result) {
+    String request = "get " + key + "\n";
+    try {
+      String response = sendRequest(request);
+      result.put("field1", new StringByteIterator(response));
+      return Status.OK;
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+
+  //Perform a range scan
+  @Override
+  public Status scan(final String table, final String startkey, final int recordcount, final Set<String> fields,
+                     final Vector<HashMap<String, ByteIterator>> result) {
+    return Status.NOT_IMPLEMENTED;
+  }
+
+  //Update a single record
+  @Override
+  public Status update(final String table, final String key, final Map<String, ByteIterator> values) {
+    return insert(table, key, values);
+  }
+
+  //Insert a single record
+  @Override
+  public Status insert(final String table, final String key, final Map<String, ByteIterator> values) {
+    StringBuilder value = new StringBuilder();
+    for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
+      value.append(entry.getValue().toString());
+    }
+    String request = "put " + key + " " + value + "\n";
+    try {
+      sendRequest(request);
+      return Status.OK;
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+
+  //Delete a single record
+  @Override
+  public Status delete(final String table, final String key) {
+    return Status.NOT_IMPLEMENTED;
+  }
+
+  private String sendRequest(String request) throws Exception {
+    writer.write(request);
+    writer.flush();
+
+    String result = reader.readLine();
+    if (Objects.equals(result, "retry") ||
+        Objects.equals(result, "leader is ...") ||
+        Objects.equals(result, "bad command")) {
+      throw new Exception();
+    }
+    return result;
+  }
+}
