b0bb85e0790be4e794060e6cbf46d41825fffe38
Zhiying Liang <zvl5490@psu.edu>
2023-03-05 22:53:44 -0500

    add script for gc measurement

---
+++ scripts/gc/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+readonly DB=$1
+readonly TEST_SETTING=$4
+readonly CLIENT_COUNT=$2
+readonly RECORD_COUNT=$3
+readonly OUTPUT_PATH=scripts/gc/${TEST_SETTING}
+mkdir -p $OUTPUT_PATH
+
+readonly WARMUP_DUARTION=10
+readonly RUN_DURATION=610
+readonly OPERATION_COUNT=$((CLIENT_COUNT * RECORD_COUNT))
+
+./bin/ycsb load $DB -P workloads/workloada \
+  -p recordcount=$RECORD_COUNT \
+  -p fieldcount=5 \
+  -threads $CLIENT_COUNT -s
+
+sleep 20
+
+./bin/ycsb run $DB -P workloads/workloada \
+    -p recordcount=$RECORD_COUNT \
+    -p operationcount=$OPERATION_COUNT \
+    -p maxexecutiontime=$RUN_DURATION \
+    -p fieldcount=5 \
+    -p writeallfields=true \
+    -p combineop=true \
+    -p measurement.interval=both \
+    -p warmup=10 \
+    -p exportercdf=true \
+    -threads $CLIENT_COUNT -s \
+    1>${OUTPUT_PATH}/stat.log 2>${OUTPUT_PATH}/status.log
